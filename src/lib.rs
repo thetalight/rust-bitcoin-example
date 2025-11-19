3ad8f636b598e0da495c6de870935864cb19693e
thetalight <156404808+thetalight@users.noreply.github.com>
2025-11-19 17:15:11 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 pub mod p2pkh;
 
 const BTC_TEST_URL: &str = "https://blockstream.info/testnet/api";
+
+// Just for test, DO NOT USE!!!
 const PRIVATE_KEY_BYTES: [u8; 32] = [
     216, 166, 206, 234, 67, 115, 17, 206, 67, 244, 2, 74, 142, 138, 59, 3, 118, 156, 69, 148, 111,
     104, 216, 47, 49, 253, 0, 104, 186, 79, 60, 224,
